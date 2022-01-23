a021c23d01297153dde8efe30f4201a784725f05
Lee, YoungHoon <nobel6018@gmail.com>
2022-01-23 22:54:36 +0900

    feat: member, team model

---
+++ src/main/java/com/example/springedatajpainflearn/SpringeDataJpaInflearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.springedatajpainflearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringeDataJpaInflearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringeDataJpaInflearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springedatajpainflearn/SpringeDataJpaInflearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springedatajpainflearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringeDataJpaInflearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
